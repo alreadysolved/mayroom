fff6b90df607b79d26563871e1d936f6871c0b8d
kye <navi332@naver.com>
2026-04-24 16:05:51 +0900

    프로젝트 생성

---
+++ src/main/java/io/github/alreadysolved/mayroom/MayroomApplication.java
@@ -0,0 +1,13 @@
+package io.github.alreadysolved.mayroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MayroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/alreadysolved/mayroom/MayroomApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alreadysolved.mayroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
